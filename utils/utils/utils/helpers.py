4de5f551167dba7dcbde295a3bc2126f087908f4
historiayguaribe-debug <historiayguaribe@gmail.com>
2026-06-23 14:33:49 -0400

    Delete utils directory

--- utils/utils/utils/helpers.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def sanitizar_texto(texto: str) -> str:
-    """Limpia caracteres especiales y espacios extra."""
-    texto = re.sub(r'\s+', ' ', texto)
-    texto = re.sub(r'[^\w\s.,!?¿¡-]', '', texto)
-    return texto.strip()
